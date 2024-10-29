c5fbf8e7cd8c0ed3663ab3eacf377ad9170404c6
Astra <105219585+astrabun@users.noreply.github.com>
2024-10-29 23:43:57 +0000

    update generate scripts

--- src/testGenerateBskyDefs.ts
+++ src/testGenerateBskyDefs.ts
@@ -1,4 +1,5 @@
 import { generateBskyDefsEnglish } from "./generateBskyDefs";
 const def = generateBskyDefsEnglish();
+console.log(`There are ${def.labelValues.length} labels in the definition`)
 console.log(def['labelValueDefinitions'].pop())
 console.log(def['labelValues'].pop())
