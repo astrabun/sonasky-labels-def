889a9894b6fc1c1d025f17c022e063885d53726c
Astra <105219585+astrabun@users.noreply.github.com>
2024-10-30 00:04:56 +0000

    update flags and update filter expression

--- src/generateBskyDefs.ts
+++ src/generateBskyDefs.ts
@@ -14,6 +14,9 @@ export const generateBskyDefs = (realm?: string, englishOnly?: boolean) => {
     let labelValues = Object.keys(onDiskDefs)
         .filter(i => onDiskDefs[i].realm === useRealm)
         .filter(i => onDiskDefs[i].flags === undefined ? true : onDiskDefs[i].flags.includes("NOT_USED_AFTER_LONG_TIME") === false) // exclude items that haven't been used since creation; e.g., someone requested the label and then never used it
+        .filter(i => onDiskDefs[i].flags === undefined ? true : ["REMOVAL_DUE_TO_"].every((flag) => {
+            return !onDiskDefs[i].flags.some(iFlag => iFlag.includes(flag))
+        })) // exclude items with specific removal flags
     ;
     let labelValueDefinitions = labelValues.map((id) => ({
         "blurs": "none",
