d7346c2297d8086a0450f41b8821f7d95f4afbf9
Astra <105219585+astrabun@users.noreply.github.com>
2024-10-30 04:56:26 +0000

    update

--- src/uploadLocalizationsToLabeler.ts
+++ src/uploadLocalizationsToLabeler.ts
@@ -4,52 +4,76 @@ import { generateRealmsOptions } from "./generateRealmsOptions";
 
 async function main() {
   const realms = generateRealmsOptions();
+  let success = true;
+  let failureError;
   // realms.forEach(async (realm: string) => {
   for (const realm of realms) {
     const defs = generateBskyDefsEnglish(realm);
-    const ozone_service_user_did = process.env[`${realm.toUpperCase()}_OZONE_SERVICE_USER_DID`] as string;
+    const ozone_service_user_did = process.env[
+      `${realm.toUpperCase()}_OZONE_SERVICE_USER_DID`
+    ] as string;
     const agent = new BskyAgent({
       service: "https://bsky.social",
     });
 
     BskyAgent.configure({
-      appLabelers: [process.env[`${realm.toUpperCase()}_OZONE_SERVICE_USER_DID`] ?? ""],
+      appLabelers: [
+        process.env[`${realm.toUpperCase()}_OZONE_SERVICE_USER_DID`] ?? "",
+      ],
     });
-    await agent.login({
-      identifier: (process.env[`${realm.toUpperCase()}_BSKY_USER`] as string).toString(),
-      password: (process.env[`${realm.toUpperCase()}_BSKY_PASS`] as string).toString(),
-    });
+    try {
+      await agent.login({
+        identifier: (
+          process.env[`${realm.toUpperCase()}_BSKY_USER`] as string
+        ).toString(),
+        password: (
+          process.env[`${realm.toUpperCase()}_BSKY_PASS`] as string
+        ).toString(),
+      });
 
-    const uploadLocalizationLabels = async () => {
-      await agent.refreshSession();
-      let body = {
-        repo: ozone_service_user_did,
-        collection: "app.bsky.labeler.service",
-        rkey: "self",
-        record: {
-          $type: "app.bsky.labeler.service",
-          policies: {
-            labelValues: defs.labelValues,
-            labelValueDefinitions: defs.labelValueDefinitions,
+      const uploadLocalizationLabels = async () => {
+        await agent.refreshSession();
+        let body = {
+          repo: ozone_service_user_did,
+          collection: "app.bsky.labeler.service",
+          rkey: "self",
+          record: {
+            $type: "app.bsky.labeler.service",
+            policies: {
+              labelValues: defs.labelValues,
+              labelValueDefinitions: defs.labelValueDefinitions,
+            },
+            createdAt: new Date().toISOString(),
           },
-          createdAt: new Date().toISOString(),
-        },
+        };
+        let response = await agent
+          .withProxy("atproto_labeler", ozone_service_user_did)
+          .api.com.atproto.repo.putRecord(body);
+        console.log(
+          `[${realm.toUpperCase()}] Upload localizations successful: `,
+          `[${response.success}]`
+        );
       };
-      let response = await agent
-        .withProxy("atproto_labeler", ozone_service_user_did)
-        .api.com.atproto.repo.putRecord(body);
-      console.log(`[${realm.toUpperCase()}] Upload localizations successful: `, `[${response.success}]`);
-    };
 
-    uploadLocalizationLabels().then(() => {
-      return "DONE"
-    }).catch((err) => {
-      throw Error(`${err}`);
-    });
-  };//);
+      uploadLocalizationLabels()
+        .then(() => {
+          return "DONE";
+        })
+        .catch((err) => {
+          throw Error(`${err}`);
+        });
+    } catch (err) {
+      success = false;
+      failureError = err;
+    }
+  } //);
+  if(!success){
+    throw failureError;
+  }
 }
 
-main().then(() => {
-}).catch((err) => {
-  throw Error(`${err}`);
-});
+main()
+  .then(() => {})
+  .catch((err) => {
+    throw Error(`${err}`);
+  });
